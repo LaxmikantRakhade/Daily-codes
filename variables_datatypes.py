5d5ceae2e276c17f8fdce3ceeabdb077d3776625
Laxmikant Rakhade <laxmikantrakhade09@gmail.com>
2026-03-20 00:50:36 +0530

    done

---
+++ variables_datatypes.py
@@ -0,0 +1,16 @@
+a = 24
+b = 3.0
+c = True
+d = 'motu'
+e = complex(2,3)
+print ('type of a : ',type(a))
+print ('type of b : ',type(b))
+print ('type of c : ',type(c))
+print ('type of d : ',type(d))
+print ('type of e : ',type(e))
+
+
+
+#list
+#touples 
+#dictonary
