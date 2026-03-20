f420c2f3ae9ddfae4117a87beca12ec91418109e
Laxmikant Rakhade <laxmikantrakhade09@gmail.com>
2026-03-21 01:06:51 +0530

    done

--- strings.py
+++ strings.py
@@ -1,32 +1,65 @@
-str1 = 'hii i am shrii'
-str2 = "hii am motu" 
+# str1 = 'hii i am shrii'
+# str2 = "hii am motu" 
 
-# multiline string """  """  ,  '''  '''
-str3 = """hiii motu 
-how are you 
-good morning
-qwertyuiop. asdfghjkl
-cvbnm 
-"""
+# # multiline string """  """  ,  '''  '''
+# str3 = """hiii motu 
+# how are you 
+# good morning
+# qwertyuiop. asdfghjkl
+# cvbnm 
+# """
 
-str4 = '''hiii motu 
-how are you 
-good morning
-qwertyui 111 222 333 444 555 op. asdfghjkl
-cvbnm ///
-'''
-print (str3)
+# str4 = '''hiii motu 
+# how are you 
+# good morning
+# qwertyui 111 222 333 444 555 op. asdfghjkl
+# cvbnm ///
+# '''
+# print (str3)
 
-print(str4[0])
-print(str4[1])
-print(str4[2])
-print(str4[3])
-print(str4[4])
+# print(str4[0])
+# print(str4[1])
+# print(str4[2])
+# print(str4[3])
+# print(str4[4])
 
-# print(str4[5123])
+# # print(str4[5123])
 
 
-# for loop 
-for character in str4 :
-    print(character)
+# # for loop 
+# for character in str4 :
+#     print(character)
+
+
+
+
+# # slicing
+string = "shrikant"
+# print (len(string))
+
+# print(string[0:5])
+# print(string[1:5])
+# print(string[1:])
+# print(string[0:5])
+# print(string[:])
+# print(string[:5])
+
+
+print(string[-6:5]) # length of string -4 to 5 (8-4=4 to 5)
+print(string[:len(string)-3])  # 0 to 8-3 ===> 0:5
+print(string[-3:-1]) # 5:7
+print(string[:]) 
+print(string[0:0]) 
+
+
+
+
+
+
+
+
+
+
+
+
 
