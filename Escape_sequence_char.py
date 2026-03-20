eed5971174b92ab6443e6f825911d029967db697
Laxmikant Rakhade <laxmikantrakhade09@gmail.com>
2026-03-20 23:42:38 +0530

    done

--- Escape_sequence_char.py
+++ Escape_sequence_char.py
@@ -4,3 +4,5 @@ print ("hey i am motu \"who are you\" and \n what are you doing here")
 print ("hey motu what are you doing \'nothing\' you ")
 
 print ('hi motu i am \'laxmikant\'fine')
+
+print ("hii motu \' how are you")
