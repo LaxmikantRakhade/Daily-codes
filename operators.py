96127b13583d0a23e5ecc596e1dbc4cf8ae07cd1
Laxmikant Rakhade <laxmikantrakhade09@gmail.com>
2026-03-20 22:51:39 +0530

    opertaors

---
+++ operators.py
@@ -0,0 +1,51 @@
+# arithmetic operators
+print(15+7)
+print(15-7)
+print(5*3)
+print(5**3)
+print(5/3)
+print(15%7)
+print(5//3)
+
+
+
+# simple calculator 
+num1 = 5
+num2 = 3 
+addition = num1+num2 
+subtraction = num1 - num2 
+multiplication = num1 * num2 
+division = num1 / num2 
+floor_division = num1 // num2 
+exponential = num1 ** num2
+modulus = num1 % num2 
+print("Adiition : ",addition)
+print("Subtraction : ",subtraction)
+print("Multiplication  : ",multiplication)
+print("Division : ",division)
+print("floor_division : ",floor_division)
+print("Exponential : ",exponential)
+print ("modulus: ",modulus)
+
+print("\n")
+
+num1 = 5
+num2 = 3
+print("The adiition of",num1 ,"+",num2,"is : ",num1 + num2 )
+print("Subtraction : ",num1 - num2 )
+print("Multiplication  : ",num1 * num2 )
+print("Division : ",num1 / num2 )
+print("floor_division : ",num1 // num2 )
+print("Exponential : ",num1 ** num2)
+print ("modulus: ",num1 % num2)
+
+
+
+
+
+
+
+
+
+
+
