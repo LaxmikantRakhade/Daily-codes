81a75b57c6bea45a65fa47055fd4108336f94fae
Laxmikant Rakhade <laxmikantrakhade09@gmail.com>
2026-03-20 23:32:50 +0530

    done

---
+++ input_function.py
@@ -0,0 +1,16 @@
+a = input()
+print(a)
+
+a = input("Enter 1st number : ")
+print (a)
+
+a = input("Enter 1st number : ")
+b = input("Enter 2nd number : ")
+print (a+b)
+
+a = input("Enter 1st number : ")
+b = input("Enter 2nd number : ")
+print ("the sum is ",int(a) + int(b))
+
+
+
