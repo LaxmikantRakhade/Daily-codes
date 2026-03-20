74da38bd0ca3331bb6a5c84e3d0f23b013b63299
Laxmikant Rakhade <laxmikantrakhade09@gmail.com>
2026-03-21 00:15:37 +0530

    done

---
+++ strings.py
@@ -0,0 +1,32 @@
+str1 = 'hii i am shrii'
+str2 = "hii am motu" 
+
+# multiline string """  """  ,  '''  '''
+str3 = """hiii motu 
+how are you 
+good morning
+qwertyuiop. asdfghjkl
+cvbnm 
+"""
+
+str4 = '''hiii motu 
+how are you 
+good morning
+qwertyui 111 222 333 444 555 op. asdfghjkl
+cvbnm ///
+'''
+print (str3)
+
+print(str4[0])
+print(str4[1])
+print(str4[2])
+print(str4[3])
+print(str4[4])
+
+# print(str4[5123])
+
+
+# for loop 
+for character in str4 :
+    print(character)
+
