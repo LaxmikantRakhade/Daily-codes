78f17e82aedcaffdfee7847600aba1c39e221a3f
Laxmikant Rakhade <laxmikantrakhade09@gmail.com>
2026-03-21 13:58:54 +0530

    done

---
+++ string_methods.py
@@ -0,0 +1,40 @@
+# string = "*** ShrikAnt **"
+
+# print (string)
+# print (len(string))
+# print (string.upper())
+# print (string.lower())
+# print (string.rstrip("*"))
+# print (string.replace("Shri","So"))
+# print (string.split("*"))
+# print (string.capitalize())
+# print (string.center(50))
+# print (len(string.center(50)))
+# print (string.count("ShrikAnt"))
+# print (string.count("*"))
+# print (string.endswith("**"))
+
+string1 = "he's is so gooD 0"
+# print (string1.endswith("is",1,7))
+
+print (string1.find("z"))
+print (string1.find("is")) 
+
+print (string1.index("is"))
+# print (string1.index("iss"))
+
+string1 = "LaxmikantRakhade192004"
+print (string1.isalnum())
+
+# string1 = "Laxmikant Rakhade 192004"
+# print (string1.isalnum())
+
+string1 = "LaxmikantRakhade"
+print (string1.isalpha())
+
+
+# string1 = "Laxmikant Rakhade"
+# print (string1.isalpha())
+
+# string1 = "LaxmikantRakhade912006"
+# print (string1.isalpha())
