cff7c4709d5c9fc2ce789578fcce4858374c06aa
Laxmikant Rakhade <laxmikantrakhade09@gmail.com>
2026-03-22 01:12:40 +0530

    done

---
+++ continue&break_statement.py
@@ -0,0 +1,14 @@
+# for i in range (13):
+#     if (i == 10):
+#         break
+#     print (i+1,"x 5 =",(i+1)*5)
+# print ("left from the loop")
+
+
+for i in range (15):
+    if (i==10):
+        print ("skip the iteration")
+        continue
+    print (i,"* 5 =",i*5)
+
+    
