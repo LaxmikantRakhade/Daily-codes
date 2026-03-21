39fb02609e427d4cc3b2f15547baab1550a0c284
Laxmikant Rakhade <laxmikantrakhade09@gmail.com>
2026-03-21 22:04:18 +0530

    done

---
+++ while_loop.py
@@ -0,0 +1,22 @@
+i = 0
+while (i<5):
+    print (i)
+    i = i+1 
+
+
+number = int (input ("Enter a number : "))
+while (number<50):
+    number = int (input("Enter a number : "))
+    print (number)
+
+print ("done with the loop !")
+
+
+
+# decrementing whilw loop
+number = 5 # -5
+while (number > 0):
+    print(number) 
+    number = number-1
+else:
+    print ("Done")
