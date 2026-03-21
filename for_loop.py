1270cc45635431fd9fe2f32494e55b733a9e73bb
Laxmikant Rakhade <laxmikantrakhade09@gmail.com>
2026-03-21 21:24:03 +0530

    done

---
+++ for_loop.py
@@ -0,0 +1,22 @@
+name = "Shrikant"
+for letters in name :
+    print(letters,end=", ")
+
+colors = ["black","white","pink","yellow","green","brown","red"]
+for color in colors:
+    print (color)
+    for i in color:
+        print (i) 
+
+
+# range function
+
+for number in range(10):
+    print (number+1) 
+
+for number in range (1,11):
+    print (number)
+
+
+for number in range (1,10,2):
+    print (number)
